6ade3c51219bc235d65244153fbcadbcf581b743
Ralph The intern <Suyat.Ralph@gmail.com>
2019-06-25 14:49:10 -0400

    Simple Dialouge Complete

--- ChatBot/app.js
+++ ChatBot/app.js
@@ -39,7 +39,6 @@ function yeetcar4(stateWrapper,ready){
 
 function MLChoice(stateWrapper,ready){
     window.open("https://i.kym-cdn.com/entries/icons/original/000/028/526/honklhonk.jpg");
-    window.location = window.location;
     ready();
 }
 // Other typa Functions
@@ -74,4 +73,39 @@ function restore(stateWrapper, ready) {
 jQuery(function($){
     convForm = $('#chat').convform({selectInputStyle: 'disable'});
     console.log(convForm);
+});
+
+jQuery(function($){
+    var count = 0;
+    var convForm = $('#chat').convform({eventList:{onInputSubmit: function(convState, ready) {
+        console.log('input is being submitted...');
+        //here you send the response to your API, get the results and build the next question
+        if(convState.current.answer.value==='end') {
+            convState.current.next = false;
+            //emulating random response time (100-600ms)
+            setTimeout(ready, Math.random()*500+100);
+        } else {
+            if(Array.isArray(convState.current.answer)) var answer = convState.current.answer.join(', ');
+            else var answer = convState.current.answer.text;
+            convState.current.next = convState.newState({
+                type: 'select',
+                noAnswer: true,
+                name: 'dynamic-question-'+count,
+                questions: ['This question state was built on your previous answer (you answered: '+answer+') and doesnt expect an answer'],
+            });
+            convState.current.next.next = convState.newState({
+                type: 'select',
+                name: 'dynamic-question-'+count,
+                questions: ['This question state was built on your previous answer (you answered: '+answer+')'],
+                answers: [
+                    {text: 'Answer 1', value: '1'},
+                    {text: 'Answer 2', value: '2'},
+                    {text: 'END', value: 'end'}
+                ]
+            });
+            //emulating random response time (100-600ms)
+            setTimeout(ready, Math.random()*500+100);
+        }
+        count++;
+    }}});
 });

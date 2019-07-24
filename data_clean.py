0596ae66bcc7b94a5bea114b7ec6c90ab2cc68df
tiago <t.ferreira@live.ca>
2019-07-24 14:41:07 -0400

    grouped data

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,4 @@
 import dask.dataframe as dd
-from glob import glob
 
 def initial():
     """
@@ -9,19 +8,18 @@ def initial():
 
     # dd.set_option('display.max_columns', None)
     df = dd.read_csv("Report.csv", assume_missing=True)
-    print(df.tail())
     mean_visit = df["Visit Number"].mean(axis=0).compute()
     # print(df.compute())
-    df = df[df["Page Name (c57) (prop57)"] != "jeep:us:"]
-    df = df[df["Page URL No Query String (c28) (prop28)"] != "https://www.jeep.com/hostc/cpov/vehicleSearch.do"]
-    df = df[df["Page URL No Query String (c28) (prop28)"] != "https://www.jeep.com/hostc/cpov/vehicleDetails.do"]
-    df = df[df["Page Type (v25) (evar25)"] != "homepage"]
+    df = df[df["Page Name"] != "jeep:us:"]
+    df = df[df["Page URL No Query String"] != "https://www.jeep.com/hostc/cpov/vehicleSearch.do"]
+    df = df[df["Page URL No Query String"] != "https://www.jeep.com/hostc/cpov/vehicleDetails.do"]
+    df = df[df["Page Type"] != "homepage"]
+
     # print(df.compute())
-    df.groupby("Page Type (v25) (evar25)")
-    print(df.compute())
+    print(df.groupby("Vehicle Model Name/Trim")["Page Views"].sum().compute().nlargest(20))
+    # print(df.compute())
     first = df[df["Visit Number"] <= mean_visit]
     second = df[df["Visit Number"] > mean_visit]
-
 
 
 if __name__ == "__main__":

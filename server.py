e9e9998ca3e9330fdbf0734bb9ffaedc9f6df5e2
ShreyansR <shreyans.rishi@uoit.net>
2019-07-25 11:47:35 -0400

    Added flask server to run locally and connect to ML

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import os
+
+template_dir = os.path.abspath('/Users/shrrishi/Documents/Personal Projects/Transport-Challenge/AutoBot')   #Put abs path on your pc
+app = Flask(__name__, template_folder=template_dir,static_folder=template_dir)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/handle_data',methods=['POST'])
+def handle_data():
+    user_input=request.form['projectFilepath']
+
+    print(user_input)
+
+
+if __name__=='__main__':
+    app.run(debug=True,port=5002)

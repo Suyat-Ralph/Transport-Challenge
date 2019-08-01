0ec3d96ff839d3cdb955b68528a711e0998c2606
tiago <t.ferreira@live.ca>
2019-08-01 16:13:30 -0400

    updated json code

--- AutoBot/app.js
+++ AutoBot/app.js
@@ -112,3 +112,4 @@ jQuery(function($){
     }}});
 });
 
+
--- server.py
+++ server.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
 
 template_dir = os.path.abspath('AutoBot')   #Put abs path on your pc
 app = Flask(__name__, template_folder=template_dir,static_folder=template_dir)
 
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+
+    # POST request
+    if request.method == 'POST':
+        input_value = request.get_json()
+        print(input_value)
+        # must return a string value and note nums is a dict of strings
+        print(str(input_value["score"]))
+
+        return "1"
+
+    # GET request
+    else:
+        message = {'greeting': 'Hello from Flask!'}
+        print("HELLO")
+        return jsonify(message)  # serialize and use JSON headers
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/handle_data',methods=['POST'])
 def handle_data():
@@ -16,4 +37,4 @@ def handle_data():
 
 
 if __name__=='__main__':
-    app.run(debug=True,port=5002)
+    app.run(debug=True, port=5002)
